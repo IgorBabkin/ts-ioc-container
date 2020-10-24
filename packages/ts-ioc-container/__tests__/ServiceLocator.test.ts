f96f18f047a7e5bf8435b4edf0bb2f717afb0adb
ibabkin <ibabkin@goodgamestudios.com>
2020-10-24 20:07:58 +0200

    fix(tests): add tests
    affects: ts-ioc-container

--- packages/ts-ioc-container/__tests__/ServiceLocator.test.ts
+++ packages/ts-ioc-container/__tests__/ServiceLocator.test.ts
@@ -4,11 +4,9 @@ import { Group } from './fixtures/Group';
 import { SubGroup3 } from './fixtures/SubGroup3';
 import { ServiceLocatorFactory } from '../lib/ServiceLocatorFactory';
 import { IInjectable } from '../lib/instanceHooks/IInjectable';
-import { FakeGenerator } from './fixtures/FakeGenerator';
 
 describe('ServiceLocator', () => {
     const locatorFactory = new ServiceLocatorFactory();
-    const fakeGenerator: FakeGenerator = new FakeGenerator();
 
     it('should create an instance', () => {
         const expectedInstance = {};
@@ -75,17 +73,8 @@ describe('ServiceLocator', () => {
             expect(child.resolve('key1')).toBe(locator.resolve('key1'));
         });
 
-        it('returns the different instalnce for child and parent', () => {
-            const locator = locatorFactory.createIoCLocator().registerFunction('key1', () => ({}));
-
-            const child = locator.createContainer();
-
-            expect(child.resolve('key1')).not.toBe(locator.resolve('key1'));
-        });
-
         it('clears container', () => {
-            const locator: IServiceLocator = locatorFactory.createIoCLocator();
-            locator.registerInstance('key1', {});
+            const locator = locatorFactory.createIoCLocator().registerInstance('key1', {});
 
             const child = locator.createContainer();
 
@@ -98,7 +87,7 @@ describe('ServiceLocator', () => {
 
         it('should invokes postConstruct', () => {
             let isPostConstructed = false;
-            const locator: IServiceLocator = locatorFactory.createIoCLocator();
+            const locator = locatorFactory.createIoCLocator();
             const disposable = {
                 postConstruct: () => {
                     isPostConstructed = true;
@@ -139,14 +128,15 @@ describe('ServiceLocator', () => {
 
             const locator = locatorFactory
                 .createIoCLocator()
-                .registerFunction('key1', () => expectedInstance, { resolving: 'perScope' });
+                .registerFunction('key1', () => expectedInstance, { resolving: 'perScope' })
+                .registerFunction('key2', () => ({}), { resolving: 'perScope' });
 
             const child1 = locator.createContainer();
             const child2 = child1.createContainer();
 
             expect(child2.resolve('key1')).toBe(expectedInstance);
-            expect(child2.resolve('key1')).toBe(expectedInstance);
             expect(child1.resolve('key1')).toBe(expectedInstance);
+            expect(child1.resolve('key2')).not.toBe(child1.resolve('key1'));
 
             expect(() => locator.resolve('key1')).toThrow();
 
@@ -160,7 +150,7 @@ describe('ServiceLocator', () => {
             expect(() => locator.resolve('key1')).toThrow();
         });
 
-        it('asd', () => {
+        it('ioc1', () => {
             const expected = ['KEY1_VALUE', 'p2', 'p3', 'KEY2_VALUE', '1', '2', 'KEY1_VALUE', 'KEY2_VALUE'];
 
             const decorated = locatorFactory
@@ -173,6 +163,25 @@ describe('ServiceLocator', () => {
             const result = group.privet();
 
             expect(result).toEqual(expected);
+        });
+
+        it('ioc2', () => {
+            const expected = {};
+
+            const locator = locatorFactory
+                .createIoCLocator()
+                .registerFunction('key1', () => expected, { resolving: 'singleton' });
+
+            const child1 = locator.createContainer();
+
+            expect(child1.resolve('key1')).toEqual(locator.resolve('key1'));
+
+            child1.remove();
+
+            const child2 = locator.createContainer();
+            expect(child2.resolve('key1')).toEqual(locator.resolve('key1'));
+
+            child2.remove();
         });
     });
 });
