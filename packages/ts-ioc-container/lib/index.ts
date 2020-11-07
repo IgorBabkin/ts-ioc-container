278a4b663737ca245d16791b29c26fc22eab0730
ibabkin <ibabkin@goodgamestudios.com>
2020-11-07 14:54:34 +0100

    feat: exports decorators
    affects: ts-ioc-container

--- packages/ts-ioc-container/lib/index.ts
+++ packages/ts-ioc-container/lib/index.ts
@@ -1,5 +1,6 @@
-export { constructor, Factory, IServiceLocator } from './IServiceLocator';
+export { constructor, IServiceLocator } from './IServiceLocator';
 export { ServiceLocator } from './ServiceLocator';
 export { ServiceLocatorDecorator } from './ServiceLocatorDecorator';
 export { ServiceLocatorFactory } from './ServiceLocatorFactory';
 export * from './IRegistration';
+export { inject, Factory, Instance } from './strategy/ioc/decorators';
