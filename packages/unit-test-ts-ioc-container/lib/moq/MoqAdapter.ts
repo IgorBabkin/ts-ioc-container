9ce043a0e890653fc983112456dcb4c21047c2d1
ibabkin <ibabkin@goodgamestudios.com>
2021-02-15 23:01:48 +0100

    feat: remove moq

--- packages/unit-test-ts-ioc-container/lib/moq/MoqAdapter.ts
+++
@@ -1,8 +0,0 @@
-import { MockAdapter } from '../MockAdapter';
-import { Mock } from 'moq.ts';
-
-export class MoqAdapter<GInstance> extends MockAdapter<Mock<GInstance>, GInstance> {
-    public getInstance(): GInstance {
-        return this.decorated.object();
-    }
-}
