5689be1eb6f77d5aebdbce04f6780428e6327d76
Igor Babkin <igba14@gmail.com>
2021-09-11 00:52:07 +0200

    refactor: cleanup

--- lib/features/ioc/IocInjector.ts
+++ lib/features/ioc/IocInjector.ts
@@ -14,10 +14,11 @@ export class IocInjector implements IInjector {
 
     resolve<T>(locator: IServiceLocator, value: constructor<T>, ...deps: any[]): T {
         const injectionFns = this.metadataCollector.getInjectionFns(value);
-        return new value(
-            ...merge(injectionFns, deps.map(constant)).map((fn) => fn(locator as IServiceLocator)),
-            this.options.simpleInjectionCompatible ? locator : undefined,
-        );
+        const args = merge(injectionFns, deps.map(constant)).map((fn) => fn(locator));
+        if (this.options.simpleInjectionCompatible) {
+            args.push(locator);
+        }
+        return new value(...args);
     }
 
     dispose(): void {}
