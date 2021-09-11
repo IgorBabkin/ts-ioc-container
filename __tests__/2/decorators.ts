26b6c9eeddda63fc273fdce446351b4039b4af9c
Igor Babkin <igba14@gmail.com>
2021-09-11 02:42:26 +0200

    refactor: cleanup

--- __tests__/2/decorators.ts
+++ __tests__/2/decorators.ts
@@ -1,5 +1,5 @@
 import 'reflect-metadata';
-import { constructor, createInjectFnDecorator, InjectFn, InjectMetadataCollector, ProviderBuilder } from '../../lib';
+import { constructor, createInjectFnDecorator, InjectMetadataCollector, ProviderBuilder } from '../../lib';
 
 export const constructorMetadataCollector = new InjectMetadataCollector(Symbol.for('CONSTRUCTOR_METADATA_KEY'));
 export const inject = createInjectFnDecorator(constructorMetadataCollector);
