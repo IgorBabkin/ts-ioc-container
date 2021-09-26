a5e21d27dc514cb8cd4f16524e67774bfc8cec7a
Igor Babkin <igba14@gmail.com>
2021-09-26 02:43:11 +0200

    fix: update api

--- packages/clean-reactive-architecture/lib/domain/ReactiveObject.ts
+++ packages/clean-reactive-architecture/lib/domain/ReactiveObject.ts
@@ -1,6 +1,6 @@
 import { BehaviorSubject, Observable } from 'rxjs';
 
-export type Reducer<T> = (value: T) => T;
+export type Reduce<T> = (value: T) => T;
 
 export class ReactiveObject<T> {
   private value$: BehaviorSubject<T>;
@@ -9,12 +9,8 @@ export class ReactiveObject<T> {
     this.value$ = new BehaviorSubject(initial);
   }
 
-  setValue(value: T): void {
-    this.value$.next(value);
-  }
-
-  update(reducer: Reducer<T>): void {
-    this.value$.next(reducer(this.value$.getValue()));
+  update(reduce: Reduce<T>): void {
+    this.value$.next(reduce(this.value$.getValue()));
   }
 
   toObservable(): Observable<T> {
