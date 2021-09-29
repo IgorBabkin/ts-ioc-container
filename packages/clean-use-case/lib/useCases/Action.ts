2bf916db11fb1e773d03547f2d953814a10ddf63
Igor Babkin <igba14@gmail.com>
2021-09-29 23:21:57 +0200

    feat: 🎸 update action

--- packages/clean-use-case/lib/useCases/Action.ts
+++ packages/clean-use-case/lib/useCases/Action.ts
@@ -1,24 +1,16 @@
-import { Observable, Subject } from 'rxjs';
+import { Subject } from 'rxjs';
 import { IAction } from './IAction';
 
 export abstract class Action<T> implements IAction<T> {
-    private _start$ = new Subject<T>();
-    private _done$ = new Subject<T>();
+    start$ = new Subject<T>();
+    done$ = new Subject<T>();
 
     dispatch(payload: T): void {
         (async () => {
-            this._start$.next(payload);
+            this.start$.next(payload);
             await this.handle(payload);
-            this._done$.next(payload);
+            this.done$.next(payload);
         })();
-    }
-
-    get done$(): Observable<T> {
-        return this._start$.asObservable();
-    }
-
-    get start$(): Observable<T> {
-        return this._start$.asObservable();
     }
 
     protected abstract handle(payload: T): Promise<void>;
