09a5eb2d0757189bd08f3600d3fad768dab6830d
Igor Babkin <igba14@gmail.com>
2021-10-07 20:03:54 +0200

    feat: 🎸 expose locator options

--- packages/react-ts-ioc-container/lib/index.ts
+++ packages/react-ts-ioc-container/lib/index.ts
@@ -1,3 +1,3 @@
 export { LocatorContext, useDependency, useLocator } from './context';
 export { useScope, Scope } from './scope';
-export { Locator, ProviderKey, InjectionToken, constructor } from './locator';
+export { Locator, ProviderKey, InjectionToken, constructor, LocatorOptions } from './locator';
