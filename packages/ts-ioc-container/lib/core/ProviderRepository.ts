12cc0c3debccd3f20467257d489f42e3324dc87e
Igor Babkin <igba14@gmail.com>
2021-10-12 18:04:23 +0200

    refactor: 💡 cleanup

--- packages/ts-ioc-container/lib/core/ProviderRepository.ts
+++ packages/ts-ioc-container/lib/core/ProviderRepository.ts
@@ -3,19 +3,18 @@ import { ProviderNotFoundError } from '../errors/ProviderNotFoundError';
 import { IProviderRepository } from './IProviderRepository';
 import { ProviderStorage } from './ProviderStorage';
 
-export class ProviderRepository implements IProviderRepository {
+export class ProviderRepository implements IProviderRepository, ScopeOptions {
     private readonly providers = new ProviderStorage();
 
-    constructor(private parent?: ProviderRepository, public level = 0, private tags: Tag[] = []) {}
+    constructor(private parent?: ProviderRepository, public level = 0, public tags: Tag[] = []) {}
 
     add<T>(key: ProviderKey, provider: IProvider<T>): void {
         this.providers.add(key, provider);
     }
 
     clone(tags: Tag[] = [], parent: ProviderRepository = this): IProviderRepository {
-        const options = { level: parent.level + 1, tags };
-        const repo = new ProviderRepository(parent, options.level, options.tags);
-        for (const [key, provider] of parent.entries(options)) {
+        const repo = new ProviderRepository(parent, parent.level + 1, tags);
+        for (const [key, provider] of parent.entries(repo)) {
             repo.add(key, provider.clone());
         }
         return repo;
@@ -36,8 +35,7 @@ export class ProviderRepository implements IProviderRepository {
     }
 
     find<T>(key: ProviderKey): IProvider<T> {
-        const options = { level: this.level, tags: this.tags };
-        const provider = this.providers.find<T>(key, options) ?? this.parent?.find<T>(key);
+        const provider = this.providers.find<T>(key, this) ?? this.parent?.find<T>(key);
         if (provider === undefined) {
             throw new ProviderNotFoundError(key.toString());
         }
