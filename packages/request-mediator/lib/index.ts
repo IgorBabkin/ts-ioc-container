abbf4f074f98e7163cdec8d128f872707beeaccc
ibabkin <igba14@gmail.com>
2023-02-03 21:21:50 +0000

    fix: 🐛 expose entities

--- packages/request-mediator/lib/index.ts
+++ packages/request-mediator/lib/index.ts
@@ -1,8 +1,10 @@
+export { IDependencyContainer } from './di/IDependencyContainer';
+
 export { RequestMediator, request } from './app/RequestMediator';
 export { useCase } from './app/UseCaseMediator';
 export { useService } from './app/ServiceMediator';
 export { Scope } from './app/Scope';
-export { ErrorHandler } from './ErrorHandler';
+export { ErrorHandler, IErrorHandler } from './ErrorHandler';
 export { IQueryHandler } from './IQueryHandler';
 export { before, after } from './mediator/simple/SimpleMediator';
-export { ITransactionContext, transaction } from './mediator/transaction/ITransactionContext';
+export { ITransactionContext, transaction, ITransactionContextKey } from './mediator/transaction/ITransactionContext';
