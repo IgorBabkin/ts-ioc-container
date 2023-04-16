284625306546af96c424a247beea9e2119d97dfa
ibabkin <igba14@gmail.com>
2023-04-16 03:21:19 +0100

    refactor: 💡 cleanp

--- packages/ts-ioc-container/lib/container/IContainer.ts
+++ packages/ts-ioc-container/lib/container/IContainer.ts
@@ -2,10 +2,10 @@ import { IProvider, ProviderKey, Tag } from '../provider/IProvider';
 import { constructor, Disposable } from '../utils/types';
 import { Registration } from '../registration/Registration';
 
-export type InjectionToken<T = any> = constructor<T> | ProviderKey;
+export type InjectionToken<T = unknown> = constructor<T> | ProviderKey;
 
 export interface Resolveable {
-    resolve<T>(key: InjectionToken<T>, ...args: any[]): T;
+    resolve<T>(key: InjectionToken<T>, ...args: unknown[]): T;
 }
 
 export interface IContainer extends Disposable, Resolveable {
