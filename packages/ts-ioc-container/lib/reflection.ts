9b7a1405b13c661f374764783d17f092d6d17050
ibabkin <igba14@gmail.com>
2023-05-25 13:31:01 +0100

    refactor: 💡 cleanup

--- packages/ts-ioc-container/lib/reflection.ts
+++ packages/ts-ioc-container/lib/reflection.ts
@@ -4,13 +4,6 @@ export const setProp =
         Reflect.defineMetadata(key, value, target);
     };
 
-export const reduceProp =
-    <T>(key: string | symbol, fn: (acc: T) => T, init: T): ClassDecorator =>
-    (target) => {
-        const current: T = Reflect.getOwnMetadata(key, target) ?? init;
-        Reflect.defineMetadata(key, fn(current), target);
-    };
-
 // eslint-disable-next-line @typescript-eslint/ban-types
 export function getProp<T>(target: Object, key: string | symbol): T | undefined {
     return Reflect.getOwnMetadata(key, target) as T;
