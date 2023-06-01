4d1cc4951be74ba2a3de54dd36420b1cda64f714
ibabkin <igba14@gmail.com>
2023-06-01 02:37:54 +0100

    fix: 🐛 ci/cd

--- packages/ts-ioc-container/jest.config.js
+++ packages/ts-ioc-container/jest.config.js
@@ -2,7 +2,13 @@
 module.exports = {
     testMatch: ['**/?(*.)+(spec|test).+(ts)'],
     transform: {
-        '^.+\\.tsx?$': 'ts-jest',
+        '^.+\\.tsx?$': [
+            'ts-jest',
+            {
+                tsConfig: './tsconfig.json',
+                isolatedModules: false,
+            },
+        ],
     },
     collectCoverage: true,
     coveragePathIgnorePatterns: ['/node_modules/', '/fixtures/', '/cjm/', '/esm/'],
