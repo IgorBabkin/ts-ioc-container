04f018502ecd1c7b5ce21b7826b383b0ae8186fc
ibabkin <igba14@gmail.com>
2023-06-13 20:57:35 +0100

    chore: add generated to gitignore

--- packages/server-interface-builder/__tests__/swagger.spec.ts
+++ packages/server-interface-builder/__tests__/swagger.spec.ts
@@ -2,11 +2,10 @@ import api from './swagger.json';
 import { OpenAPIV3 } from 'openapi-types';
 import { renderDocument } from '../lib';
 import fs from 'fs';
-import path from 'path';
 
 describe('swagger', function () {
     it('ServerGenerator', function () {
-        fs.writeFileSync(path.resolve(__dirname, './output.d.ts'), renderDocument(api as OpenAPIV3.Document));
+        fs.writeFileSync('.generated/output.d.ts', renderDocument(api as OpenAPIV3.Document));
         expect(true).toEqual(true);
     });
 });
--- packages/zod-builder/__tests__/swagger.spec.ts
+++ packages/zod-builder/__tests__/swagger.spec.ts
@@ -2,11 +2,10 @@ import api from './swagger.json';
 import { OpenAPIV3 } from 'openapi-types';
 import { renderDocument } from '../lib';
 import * as fs from 'fs';
-import * as path from 'path';
 
 describe('swagger', function () {
     it('Generator', function () {
-        fs.writeFileSync(path.resolve(__dirname, './output.ts'), renderDocument(api as OpenAPIV3.Document));
+        fs.writeFileSync('.generated/output.ts', renderDocument(api as OpenAPIV3.Document));
         expect(true).toEqual(true);
     });
 });
