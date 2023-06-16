8577cb3951f1e9724989d83afaf5051adc9bcd81
ibabkin <igba14@gmail.com>
2023-06-16 02:21:11 +0100

    fix: 🐛 asd

--- packages/openapi-to-server/lib/openapi-to-server.ts
+++ packages/openapi-to-server/lib/openapi-to-server.ts
@@ -6,6 +6,7 @@ import * as fs from 'fs';
 import { renderServer } from './index';
 import { OpenAPIV3 } from 'openapi-types';
 import yaml from 'js-yaml';
+import * as console from 'console';
 
 args.option('input', 'openapi file path').option('output', 'output file path');
 
@@ -23,6 +24,7 @@ if (!flags.output) {
 
 const data: string = fs.readFileSync(inputFile, { encoding: 'utf-8' });
 const content: OpenAPIV3.Document = inputFile.search(/\.json$/) > 0 ? JSON.parse(data) : (yaml.load(data) as any);
+console.log(content);
 fs.writeFileSync(outputFile, renderServer(content), { encoding: 'utf-8' });
 
 process.exit(0);
