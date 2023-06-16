bb8ee5683a96af3e4affe985e04023e6f4bd7b8d
ibabkin <igba14@gmail.com>
2023-06-16 02:37:42 +0100

    feat: 🎸 support yaml

--- packages/openapi-to-zod/lib/openapi-to-zod.ts
+++ packages/openapi-to-zod/lib/openapi-to-zod.ts
@@ -4,6 +4,8 @@ import * as process from 'process';
 import args from 'args';
 import * as fs from 'fs';
 import { renderDocument } from './index';
+import { OpenAPIV3 } from 'openapi-types';
+import yaml from 'js-yaml';
 
 args.option('input', 'openapi file path').option('output', 'output file path');
 
@@ -19,7 +21,9 @@ if (!flags.output) {
     throw new Error('output file path is required');
 }
 
-const openapi = JSON.parse(fs.readFileSync(inputFile, { encoding: 'utf-8' }));
-fs.writeFileSync(outputFile, renderDocument(openapi), { encoding: 'utf-8' });
+const data: string = fs.readFileSync(inputFile, { encoding: 'utf-8' });
+const content: OpenAPIV3.Document = inputFile.search(/\.json$/) > 0 ? JSON.parse(data) : (yaml.load(data) as any);
+console.log(JSON.stringify(content, null, 2));
+fs.writeFileSync(outputFile, renderDocument(content), { encoding: 'utf-8' });
 
 process.exit(0);
