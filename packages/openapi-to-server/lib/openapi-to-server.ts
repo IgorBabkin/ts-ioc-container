f2975a93dc612b2993596ee9a634dd236a11548a
ibabkin <igba14@gmail.com>
2023-06-16 01:36:00 +0100

    feat: 🎸 support yaml

--- packages/openapi-to-server/lib/openapi-to-server.ts
+++ packages/openapi-to-server/lib/openapi-to-server.ts
@@ -4,6 +4,8 @@ import * as process from 'process';
 import args from 'args';
 import * as fs from 'fs';
 import { renderServer } from './index';
+import { OpenAPIV3 } from 'openapi-types';
+import yaml from 'js-yaml';
 
 args.option('input', 'openapi file path').option('output', 'output file path');
 
@@ -19,7 +21,7 @@ if (!flags.output) {
     throw new Error('output file path is required');
 }
 
-const openapi = JSON.parse(fs.readFileSync(inputFile, { encoding: 'utf-8' }));
+const openapi: OpenAPIV3.Document = yaml.load(fs.readFileSync(inputFile, { encoding: 'utf-8' })) as any;
 fs.writeFileSync(outputFile, renderServer(openapi), { encoding: 'utf-8' });
 
 process.exit(0);
