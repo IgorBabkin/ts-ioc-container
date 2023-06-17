d8ae2daad605c918a5867a89cca95ae0991451a3
ibabkin <igba14@gmail.com>
2023-06-17 18:55:16 +0100

    refactor: 💡 cleanup

--- packages/openapi-to-zod/lib/openapi-to-zod.ts
+++ packages/openapi-to-zod/lib/openapi-to-zod.ts
@@ -2,28 +2,35 @@
 
 import * as process from 'process';
 import args from 'args';
-import * as fs from 'fs';
 import { renderDocument } from './index';
 import { OpenAPIV3 } from 'openapi-types';
-import yaml from 'js-yaml';
+import { isYAML, loadYAML } from './utils/yaml';
+import { loadJSON } from './utils/json';
+import { saveFile } from './utils/file';
 
-args.option('input', 'openapi file path').option('output', 'output file path');
+function parseArguments(props: string[]) {
+    args.option('input', 'openapi file path').option('output', 'output file path');
 
-const flags = args.parse(process.argv);
+    const flags = args.parse(props);
 
-const inputFile = flags.input;
-if (!inputFile) {
-    throw new Error('openapi file path is required');
+    const inputFile = flags.input;
+    if (!inputFile) {
+        throw new Error('openapi file path is required');
+    }
+
+    const outputFile = flags.output;
+    if (!flags.output) {
+        throw new Error('output file path is required');
+    }
+
+    return { inputFile, outputFile };
 }
 
-const outputFile = flags.output;
-if (!flags.output) {
-    throw new Error('output file path is required');
+function main(props: string[]) {
+    const { inputFile, outputFile } = parseArguments(props);
+    const content: OpenAPIV3.Document = isYAML(inputFile) ? loadYAML(inputFile) : loadJSON(inputFile);
+    saveFile(outputFile, renderDocument(content));
 }
 
-const data: string = fs.readFileSync(inputFile, { encoding: 'utf-8' });
-const content: OpenAPIV3.Document = inputFile.search(/\.json$/) > 0 ? JSON.parse(data) : (yaml.load(data) as any);
-console.log(JSON.stringify(content, null, 2));
-fs.writeFileSync(outputFile, renderDocument(content), { encoding: 'utf-8' });
-
+main(process.argv);
 process.exit(0);
---
+++ packages/openapi-to-zod/lib/utils/json.ts
@@ -0,0 +1,6 @@
+import fs from 'fs';
+
+export function loadJSON<T>(inputFile: string): T {
+    const data: string = fs.readFileSync(inputFile, { encoding: 'utf-8' });
+    return JSON.parse(data);
+}
