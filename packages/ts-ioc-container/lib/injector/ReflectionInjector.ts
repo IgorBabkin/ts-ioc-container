f199fed447745d823af57a28a099c2815878addb
ibabkin <igba14@gmail.com>
2023-08-26 21:43:44 +0100

    refactor: 💡 update node version

--- packages/ts-ioc-container/lib/injector/ReflectionInjector.ts
+++ packages/ts-ioc-container/lib/injector/ReflectionInjector.ts
@@ -5,22 +5,19 @@ import { getProp } from '../reflection';
 
 type InjectFn<T = unknown> = (l: IContainer, ...args: unknown[]) => T;
 
-const attr =
-  (key: string | symbol) =>
-  (value: InjectFn): ParameterDecorator =>
-  (target, propertyKey, parameterIndex) => {
-    const metadata = Reflect.getOwnMetadata(key, target) ?? [];
-    metadata[parameterIndex] = value;
-    Reflect.defineMetadata(key, metadata, target);
-  };
-
 export const by =
   <T>(key: InjectionToken<T>, ...args: unknown[]) =>
   (c: IContainer) =>
     c.resolve<T>(key, ...args);
 
 const METADATA_KEY = 'INJECT_FN_LIST';
-export const inject = attr(METADATA_KEY);
+export const inject =
+  (value: InjectFn): ParameterDecorator =>
+  (target, propertyKey, parameterIndex) => {
+    const metadata = Reflect.getOwnMetadata(METADATA_KEY, target) ?? [];
+    metadata[parameterIndex] = value;
+    Reflect.defineMetadata(METADATA_KEY, metadata, target);
+  };
 
 export class ReflectionInjector implements IInjector {
   resolve<T>(container: IContainer, Target: constructor<T>, ...deps: unknown[]): T {
