a2b26fce469804e21d88b77a6ecb57519fe45dae
ibabkin <igba14@gmail.com>
2023-09-03 18:20:09 +0100

    docs: ✏️ readme

--- packages/ts-ioc-container/__tests__/TaggedProvider.spec.ts
+++ packages/ts-ioc-container/__tests__/TaggedProvider.spec.ts
@@ -2,13 +2,11 @@ import 'reflect-metadata';
 import { singleton, Container, key, tags, provider, ReflectionInjector, Registration } from 'ts-ioc-container';
 
 @key('ILogger')
-@provider(singleton(), tags('root'))
+@provider(singleton(), tags('root')) // the same as .pipe(singleton(), tags('root'))
 class Logger {}
 describe('TaggedProvider', function () {
   it('should return the same instance', function () {
-    const root = new Container(new ReflectionInjector(), { tags: ['root'] }).add(
-      Registration.fromClass(Logger).pipe(tags('root', 'parent')),
-    );
+    const root = new Container(new ReflectionInjector(), { tags: ['root'] }).add(Registration.fromClass(Logger));
     const child = root.createScope();
     expect(root.resolve('ILogger')).toBe(child.resolve('ILogger'));
   });
