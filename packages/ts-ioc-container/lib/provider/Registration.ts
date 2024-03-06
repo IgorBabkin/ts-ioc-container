05824a1cbe5ee1b92f91245464cbc5daad5135c0
ibabkin <igba14@gmail.com>
2024-03-06 20:57:28 +0000

    refactor: 💡 cleanup

---
+++ packages/ts-ioc-container/lib/provider/Registration.ts
@@ -0,0 +1,44 @@
+import { DependencyKey, IContainer } from '../container/IContainer';
+import { constructor, MapFn } from '../utils';
+import { getMetadata, setMetadata } from '../metadata';
+import { Provider } from './Provider';
+import { IProvider, ResolveDependency } from './IProvider';
+import { DependencyMissingKeyError } from '../errors/DependencyMissingKeyError';
+
+const DEPENDENCY_KEY = 'DependencyKey';
+
+export const key = (value: DependencyKey) => setMetadata(DEPENDENCY_KEY, value);
+
+export class Registration<T> {
+  static fromClass<T>(Target: constructor<T>) {
+    const dependencyKey = getMetadata<DependencyKey>(Target, DEPENDENCY_KEY);
+    return new Registration(Provider.fromClass(Target), dependencyKey ?? Target.name);
+  }
+
+  static fromValue<T>(value: T) {
+    return new Registration(Provider.fromValue(value));
+  }
+
+  static fromFn<T>(fn: ResolveDependency<T>) {
+    return new Registration(new Provider(fn));
+  }
+
+  constructor(private provider: IProvider<T>, private key?: DependencyKey) {}
+
+  to(key: DependencyKey): this {
+    this.key = key;
+    return this;
+  }
+
+  pipe(...mappers: MapFn<IProvider<T>>[]): this {
+    this.provider = this.provider.pipe(...mappers);
+    return this;
+  }
+
+  applyTo(container: IContainer): void {
+    if (!this.key) {
+      throw new DependencyMissingKeyError('No key provided for registration');
+    }
+    container.register(this.key, this.provider);
+  }
+}
