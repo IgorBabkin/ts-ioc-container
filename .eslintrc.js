11463754d633ab368e4602386a02e31deba4ca29
ibabkin <igba14@gmail.com>
2024-03-28 20:35:50 +0100

    refactor: 💡 upgrade linters

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,19 +1,20 @@
 /* eslint-disable */
 module.exports = {
-    root: true,
-    parser: '@typescript-eslint/parser',
-    plugins: ['@typescript-eslint'],
-    extends: [
-        'eslint:recommended',
-        'plugin:@typescript-eslint/eslint-recommended',
-        'plugin:@typescript-eslint/recommended',
-        'plugin:prettier/recommended',
-    ],
-    rules: {
-        '@typescript-eslint/no-explicit-any': 0,
-        '@typescript-eslint/no-empty-function': 0,
-        '@typescript-eslint/explicit-module-boundary-types': 0,
-        '@typescript-eslint/ban-ts-comment': 0,
-        '@typescript-eslint/explicit-member-accessibility': ['error', { accessibility: 'no-public' }],
-    },
+  root: true,
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+    'prettier',
+  ],
+  rules: {
+    '@typescript-eslint/no-explicit-any': 0,
+    '@typescript-eslint/no-empty-function': 0,
+    '@typescript-eslint/explicit-module-boundary-types': 0,
+    '@typescript-eslint/ban-ts-comment': 0,
+    '@typescript-eslint/explicit-member-accessibility': ['error', { accessibility: 'no-public' }],
+  },
 };
