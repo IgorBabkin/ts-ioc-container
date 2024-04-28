947bcc714041ed8681968826d18bbe5db0fa3ccd
ibabkin <igba14@gmail.com>
2024-04-28 23:22:28 +0100

    feat: 🎸 server
    add context

--- packages/openapi-to-server/lib/utils/http.ts
+++ packages/openapi-to-server/lib/utils/http.ts
@@ -2,22 +2,13 @@ export interface RouteOptions {
   tags: string[];
 }
 
-export interface HttpResponse<Payload = unknown> {
+export interface HttpHeaders {
+  Location: string;
+}
+
+export interface HttpResponse {
   status: number;
-  location?: string;
-  payload?: Payload;
-}
-
-export interface Ok<Payload> extends HttpResponse<Payload> {
-  status: 200;
-}
-
-export interface Created extends HttpResponse {
-  status: 201;
-}
-
-export interface NoContent extends HttpResponse {
-  status: 204;
+  headers: Partial<HttpHeaders>;
 }
 
 export interface Route<Payload, Response extends HttpResponse> {
