156cdc46a6bd36e8343f3ce750ccf2f0cca60b60
ibabkin <igba14@gmail.com>
2024-04-28 18:01:37 +0100

    refactor: 💡 server
    routes

--- packages/openapi-to-server/lib/utils/http.ts
+++ packages/openapi-to-server/lib/utils/http.ts
@@ -2,23 +2,24 @@ export interface RouteOptions {
   tags: string[];
 }
 
-export interface Route<Payload, Response> {
-  handle(payload: Payload, options: RouteOptions): Promise<Response>;
-}
-
 export interface HttpResponse<Payload = unknown> {
   status: number;
-  payload: Payload;
+  location?: string;
+  payload?: Payload;
 }
 
 export interface Ok<Payload> extends HttpResponse<Payload> {
   status: 200;
 }
 
-export interface Created extends HttpResponse<undefined> {
+export interface Created extends HttpResponse {
   status: 201;
 }
 
-export interface NoContent extends HttpResponse<undefined> {
+export interface NoContent extends HttpResponse {
   status: 204;
+}
+
+export interface Route<Payload, Response extends HttpResponse> {
+  handle(payload: Payload, options: RouteOptions): Promise<Response>;
 }
