908cdfbdbcde2da48b30a475f0574122386c1035
ibabkin <igba14@gmail.com>
2024-05-08 13:09:19 +0100

    feat: 🎸 hook
    support list of executions

--- packages/ts-ioc-container/lib/hooks/hook.ts
+++ packages/ts-ioc-container/lib/hooks/hook.ts
@@ -3,19 +3,19 @@ import { ExecutionContext } from './ExecutionContext';
 
 export type Execution = <T extends ExecutionContext>(context: T) => void;
 
-const createStore = () => new Map<string, Execution>();
+const createStore = () => new Map<string | symbol, Execution[]>();
 
 export const hook =
-  (key: string | symbol, fn: Execution): MethodDecorator =>
+  (key: string | symbol, ...fns: Execution[]): MethodDecorator =>
   (target, propertyKey) => {
-    const hooks = Reflect.hasMetadata(key, target.constructor)
+    const hooks: Map<string | symbol, Execution[]> = Reflect.hasMetadata(key, target.constructor)
       ? Reflect.getMetadata(key, target.constructor)
       : createStore();
-    hooks.set(propertyKey, fn);
+    hooks.set(propertyKey, (hooks.get(propertyKey) ?? []).concat(fns));
     Reflect.defineMetadata(key, hooks, target.constructor); // eslint-disable-line @typescript-eslint/ban-types
   };
 
-export function getHooks(target: object, key: string | symbol): Map<string, Execution> {
+export function getHooks(target: object, key: string | symbol): Map<string, Execution[]> {
   return Reflect.hasMetadata(key, target.constructor) ? Reflect.getMetadata(key, target.constructor) : new Map();
 }
 
@@ -31,7 +31,9 @@ export const executeHooks = <Context extends ExecutionContext>(
     createContext = (c) => c as Context,
   }: { scope: IContainer; createContext?: (c: ExecutionContext) => Context },
 ) => {
-  for (const [methodName, execute] of getHooks(target, key)) {
-    execute(createContext(new ExecutionContext(target, methodName, scope)));
+  for (const [methodName, executions] of getHooks(target, key)) {
+    for (const execute of executions) {
+      execute(createContext(new ExecutionContext(target, methodName, scope)));
+    }
   }
 };
