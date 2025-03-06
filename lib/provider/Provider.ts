976893ce07d130d400c669f72277139a35ca5672
Igor Babkin <igba14@gmail.com>
2025-03-07 00:52:49 +0100

    fix(asd): sad
    asd

--- lib/provider/Provider.ts
+++ lib/provider/Provider.ts
@@ -24,7 +24,6 @@ export class Provider<T = any> implements IProvider<T> {
     return new Provider<T>((c) => c.resolve(key));
   }
 
-  key?: DependencyKey;
   private readonly aliases: Set<Alias> = new Set();
   private argsFn: ArgsFn = () => [];
   private isVisibleWhen: ChildrenVisibilityPredicate = () => true;
