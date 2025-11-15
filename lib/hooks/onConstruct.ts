b35a3a386690f81eee37a30545dbd16f1d208e37
Igor Babkin <igba14@gmail.com>
2025-11-15 02:58:39 +0100

    feat(hooks): update ondispose and onconstruct hooks

--- lib/hooks/onConstruct.ts
+++ lib/hooks/onConstruct.ts
@@ -1,10 +1,10 @@
-import { hook, HookFn } from './hook';
+import { hook, HookClass, HookFn } from './hook';
 import { SyncHooksRunner } from './runner/SyncHooksRunner';
 import type { IContainer } from '../container/IContainer';
 
-import { Instance } from '../types';
+import { constructor, Instance } from '../types';
 
 export const onConstructHooksRunner = new SyncHooksRunner('onConstruct');
-export const onConstruct = (fn: HookFn) => hook('onConstruct', fn);
+export const onConstruct = (fn: HookFn | constructor<HookClass>) => hook('onConstruct', fn);
 
 export type OnConstructHook = (instance: Instance, scope: IContainer) => void;
--- lib/hooks/onDispose.ts
+++ lib/hooks/onDispose.ts
@@ -1,10 +1,9 @@
-import { hook } from './hook';
+import { hook, HookClass, HookFn } from './hook';
 import { SyncHooksRunner } from './runner/SyncHooksRunner';
 import type { IContainer } from '../container/IContainer';
+import { constructor } from '../types';
 
 export const onDisposeHooksRunner = new SyncHooksRunner('onDispose');
-export const onDispose = hook('onDispose', (context) => {
-  context.invokeMethod({});
-});
+export const onDispose = (fn: HookFn | constructor<HookClass>) => hook('onDispose', fn);
 
 export type OnDisposeHook = (scope: IContainer) => void;
