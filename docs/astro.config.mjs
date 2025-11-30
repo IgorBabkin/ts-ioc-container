5c0994c421678e3521af7c4c282252e3c470995b
Igor Babkin <igba14@gmail.com>
2025-11-30 01:24:45 +0100

    docs(pages): fix css

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -1,13 +1,11 @@
 import { defineConfig } from "astro/config";
 import sitemap from "@astrojs/sitemap";
 
-import react from "@astrojs/react";
-
 // https://astro.build/config
 export default defineConfig({
   site: "https://igorbabkin.github.io",
   base: "/ts-ioc-container",
-  integrations: [sitemap(), react()],
+  integrations: [sitemap()],
   vite: {
     build: {
       // Use esnext to avoid ES2024 warnings
